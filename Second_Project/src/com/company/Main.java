d9150bbcaff2fdfc8a35413e4b8c7c35d0440d6b
jeonja <git@github.com:jeonja/totoo.git>
2022-03-03 14:26:55 +0530

    Commit for the Second Project Through IDE for the First Time

---
+++ Second_Project/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc=new Scanner(System.in);
+        String str="";
+        System.out.println("Please Enter a String");
+        str=sc.nextLine();
+        System.out.println(str);
+    }
+}
